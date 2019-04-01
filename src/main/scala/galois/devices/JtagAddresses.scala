47f6b55eb288b642f40a9d46d9b7862d630b3203
Max Waugaman <mwaugaman@galois.com>
2019-04-01 15:15:43 -0700

    Changed JTAG to work with Xilinx

---
+++ src/main/scala/galois/devices/JtagAddresses.scala
@@ -0,0 +1,11 @@
+
+package freechips.rocketchip.galois.devices
+
+import freechips.rocketchip.devices.debug.dtmJTAGAddrs
+
+// Custom implementation of dtmJtagAddrs to support our particular instantiation of Xilinx BSCANE primitives
+class xilinxAddrs extends dtmJTAGAddrs (
+  IDCODE       = 0x002924,
+  DTM_INFO     = 0x022924,
+  DMI_ACCESS   = 0x003924
+)
