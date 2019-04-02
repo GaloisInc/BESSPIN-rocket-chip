0adf93d70a3308aff4351bc7bf3569d619cc32d2
Max Waugaman <mwaugaman@galois.com>
2019-04-02 12:00:54 -0700

    Updated clint, MMIO, and MEM adresses

--- src/main/scala/subsystem/Configs.scala
+++ src/main/scala/subsystem/Configs.scala
@@ -301,8 +301,8 @@ class WithTimebase(hertz: BigInt) extends Config((site, here, up) => {
 
 class WithDefaultMemPort extends Config((site, here, up) => {
   case ExtMem => Some(MasterPortParams(
-                      base = x"8000_0000",
-                      size = x"1000_0000",
+                      base = x"C000_0000",
+                      size = x"4000_0000",
                       beatBytes = site(MemoryBusKey).beatBytes,
                       idBits = 4))
 })
@@ -313,10 +313,14 @@ class WithNoMemPort extends Config((site, here, up) => {
 
 class WithDefaultMMIOPort extends Config((site, here, up) => {
   case ExtBus => Some(MasterPortParams(
-                      base = x"6000_0000",
-                      size = x"2000_0000",
+                      base = x"2000_0000",
+                      size = x"a000_0000",
                       beatBytes = site(MemoryBusKey).beatBytes,
                       idBits = 4))
+})
+
+class WithGFECLINT extends Config((site, here, up) => {
+  case CLINTKey => Some(CLINTParams(baseAddress = x"1000_0000"))
 })
 
 class WithNoMMIOPort extends Config((site, here, up) => {
